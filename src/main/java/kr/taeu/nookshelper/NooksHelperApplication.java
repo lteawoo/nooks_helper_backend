f3f06f6a814a8a7fb3636d89894124469daae9e9
Taewoo Lee <lteawoo@naver.com>
2020-05-06 19:55:51 +0900

    init

---
+++ src/main/java/kr/taeu/nookshelper/NooksHelperApplication.java
@@ -0,0 +1,13 @@
+package kr.taeu.nookshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooksHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooksHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/taeu/nookshelper/NooksHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.taeu.nookshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooksHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
